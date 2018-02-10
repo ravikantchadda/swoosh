e60b0f202b88e26c11df3bdf46dab6416d9f4155
Ravi Kant Chadda <er.ravi.iosdeveloper@gmail.com>
2018-02-10 11:45:58 +0530

    lession 2

--- app/src/main/java/com/development/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/development/swoosh/WelcomeActivity.kt
@@ -3,7 +3,7 @@ package com.development.swoosh
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
